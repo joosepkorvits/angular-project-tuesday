7f9faea021dab4162e4bfcc44e9629fc064a763d
Joosep Kõrvits <joosepk@datafox.ee>
2024-04-09 12:05:38 +0300

    commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
